502f5aed253c64f51386f13d04dbfb9b44712e53
黄智彬 <ikrboy@163.com>
2018-12-28 14:50:47 +0800

    no message

---
+++ Mapard/Mapard/BaseModel.swift
@@ -0,0 +1,17 @@
+//
+//  BaseModel.swift
+//  Acupuncture
+//
+//  Created by BinHuang on 2018/9/18.
+//  Copyright © 2018年 BinHuang. All rights reserved.
+//
+
+import UIKit
+
+class BaseModel: NSObject {
+    
+    required init(coder aDecoder: NSCoder?) {
+        super.init();
+    }
+
+}
